0e89d8004574b0ef89d5f7e511d67f4971a223f6
Israiloff <stardustindark@gmail.com>
2023-05-07 20:54:46 +0500

    added: sample wpf app, nuget spec for init impl project;
    Signed-off-by: Israiloff <stardustindark@gmail.com>

--- InitializerImpl/Properties/AssemblyInfo.cs
+++ InitializerImpl/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("InitializerImpl")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Initialization point for MMVM")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Israiloff")]
 [assembly: AssemblyProduct("InitializerImpl")]
@@ -31,5 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.*")]
---
+++ SampleWpfApp/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleWpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
