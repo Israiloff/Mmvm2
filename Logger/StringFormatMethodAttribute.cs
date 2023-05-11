441e3c0a71f80eb71c6fbba901fb73c712bede8e
Israiloff <stardustindark@gmail.com>
2023-05-11 16:24:11 +0500

    fixed: issues with dependencies and jetbrains' annotations related issues of the logger project;
    Signed-off-by: Israiloff <stardustindark@gmail.com>

---
+++ Logger/StringFormatMethodAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace JetBrains.Annotations
+{
+    [AttributeUsage(
+        AttributeTargets.Constructor | AttributeTargets.Method |
+        AttributeTargets.Property | AttributeTargets.Delegate)]
+    public class StringFormatMethodAttribute : Attribute
+    {
+        /// <param name="formatParameterName">
+        /// Specifies which parameter of an annotated method should be treated as the format string
+        /// </param>
+        public StringFormatMethodAttribute(string formatParameterName)
+        {
+            FormatParameterName = formatParameterName;
+        }
+
+        public string FormatParameterName { get; }
+    }
+}
